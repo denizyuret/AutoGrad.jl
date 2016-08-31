e42ed346f59404f63052c67e84d08e52b9d4e4d5
Deniz Yuret <denizyuret@gmail.com>
2016-08-31 15:44:27 +0300

    reduce.jl added support for tuple sums.

--- src/base/reduce.jl
+++ src/base/reduce.jl
@@ -1,4 +1,5 @@
 @primitive  sum(x),dy  (convert(eltype(x),getval(dy)).+zeros(x))
+@primitive  sum(x::Tuple),dy  ntuple(i->dy,length(x))
 @primitive  sum(x,i...),dy  (dy.+zeros(x))
 fixdomain(::Fn{:sum},x...)=(rand()<0.5 ? (rand(2,2),) : (rand(2,2),1))
 
