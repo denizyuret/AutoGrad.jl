f48b842bab18a73696abfaa418081767dbe3df70
Deniz Yuret <denizyuret@gmail.com>
2016-09-03 11:08:23 +0300

    erf.jl completed.

--- src/special/erf.jl
+++ src/special/erf.jl
@@ -1,15 +1,14 @@
-erf1arg = Dict{Symbol,Any}(
-#:dawson => :todo,  # erf,operators
-#:erfcinv => :todo,  # erf,operators
-#:erfcx => :todo,    # erf,operators
-#:erfi => :todo,     # erf,operators
-#:erfinv => :todo,   # erf,operators
-)
+erf1arg = [
+(erf, :(exp(-abs2(x))*2/sqrt(pi))),     # \frac{2}{\sqrt{\pi}} \int_0^x e^{-t^2} dt
+(erfc, :(-exp(-abs2(x))*2/sqrt(pi))),   # 1-erf(x)
+(erfcx, :(2y.*x-2/sqrt(pi))),           # erfc(x)*exp(x^2)
+(erfi, :(exp(abs2(x))*2/sqrt(pi))),     # -i*erf(ix)
+(dawson, :(-2y.*x+1)),                  # \frac{\sqrt{\pi}}{2} e^{-x^2} erfi(x).
+(erfinv, :(exp(abs2(y))*sqrt(pi)/2)),   # erf(erfinv(x)) = x
+(erfcinv, :(-exp(abs2(y))*sqrt(pi)/2)), # erfc(erfcinv(x)) = x
+]
 
-
-# TODO:
-
-# eval
-# $(Expr(:$, :fname)): Not a symbol
-# erfinv
-# erfcinv
+for (f,g) in erf1arg
+    @eval @primitive $f(x),dy,y  (dy.*($g))
+    addtest(f,randn()); addtest(f,randn(2))
+end
