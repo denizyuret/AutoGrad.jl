1b9be856d04493f3ed99c259a9ff90d3f90b4839
Deniz Yuret <denizyuret@gmail.com>
2016-09-03 12:28:15 +0300

    Added domain info to erf.jl

--- src/special/erf.jl
+++ src/special/erf.jl
@@ -1,14 +1,22 @@
 erf1arg = [
-(erf, :(exp(-abs2(x))*2/sqrt(pi))),     # \frac{2}{\sqrt{\pi}} \int_0^x e^{-t^2} dt
-(erfc, :(-exp(-abs2(x))*2/sqrt(pi))),   # 1-erf(x)
-(erfcx, :(2y.*x-2/sqrt(pi))),           # erfc(x)*exp(x^2)
-(erfi, :(exp(abs2(x))*2/sqrt(pi))),     # -i*erf(ix)
-(dawson, :(-2y.*x+1)),                  # \frac{\sqrt{\pi}}{2} e^{-x^2} erfi(x).
-(erfinv, :(exp(abs2(y))*sqrt(pi)/2)),   # erf(erfinv(x)) = x
-(erfcinv, :(-exp(abs2(y))*sqrt(pi)/2)), # erfc(erfcinv(x)) = x
+(erf, :(exp(-abs2(x))*2/sqrt(pi)), (-Inf,Inf)),     # \frac{2}{\sqrt{\pi}} \int_0^x e^{-t^2} dt
+(erfc, :(-exp(-abs2(x))*2/sqrt(pi)), (-Inf,Inf)),   # 1-erf(x)
+(erfcx, :(2y.*x-2/sqrt(pi)), (-Inf,Inf)),           # erfc(x)*exp(x^2)
+(erfi, :(exp(abs2(x))*2/sqrt(pi)), (-Inf,Inf)),     # -i*erf(ix)
+(dawson, :(-2y.*x+1), (-Inf,Inf)),                  # \frac{\sqrt{\pi}}{2} e^{-x^2} erfi(x).
+(erfinv, :(exp(abs2(y))*sqrt(pi)/2), (-1,1)),       # erf(erfinv(x)) = x
+(erfcinv, :(-exp(abs2(y))*sqrt(pi)/2), (0,2)),      # erfc(erfcinv(x)) = x
 ]
 
-for (f,g) in erf1arg
+for (f,g,r) in erf1arg
     @eval @primitive $f(x),dy,y  (dy.*($g))
-    addtest(f,randn()); addtest(f,randn(2))
+    if r==(-Inf,Inf)
+        addtest(f,randn()); addtest(f,randn(2))
+    elseif r==(-1,1)
+        addtest(f,-1+2rand()); addtest(f,-1+2rand(2))
+    elseif r==(0,2)
+        addtest(f,2rand()); addtest(f,2rand(2))
+    else
+        error("Unknown range $r")
+    end
 end
