bdf688fe16cf5fae4668d3033621715985c26701
Deniz Yuret <denizyuret@gmail.com>
2016-09-03 17:54:50 +0300

    Fixed bessel domains.

--- src/special/bessel.jl
+++ src/special/bessel.jl
@@ -1,20 +1,20 @@
 # work in progress...
 
 bessel1arg = [
-(airyai, :(airyaiprime(x))),
-(airyaiprime, :(x.*airyai(x))),
-(airybi, :(airybiprime(x))),
-(airybiprime, :(x.*airybi(x))),
-(airyprime, :(x.*airyai(x))),
-(besselj0, :(-besselj1(x))),
-(besselj1, :((besselj0(x)-besselj(2,x))/2)),
-(bessely0, :(-bessely1(x))),
-(bessely1, :((bessely0(x)-bessely(2,x))/2)),
+(airyai, :(airyaiprime(x)), (-Inf,Inf)),
+(airyaiprime, :(x.*airyai(x)), (-Inf,Inf)),
+(airybi, :(airybiprime(x)), (-Inf,Inf)),
+(airybiprime, :(x.*airybi(x)), (-Inf,Inf)),
+(airyprime, :(x.*airyai(x)), (-Inf,Inf)),
+(besselj0, :(-besselj1(x)), (-Inf,Inf)),
+(besselj1, :((besselj0(x)-besselj(2,x))/2), (-Inf,Inf)),
+(bessely0, :(-bessely1(x)), (0,Inf)), 
+(bessely1, :((bessely0(x)-bessely(2,x))/2), (0,Inf)),
 ]
 
-for (f,g) in bessel1arg
+for (f,g,r) in bessel1arg
     @eval @primitive $f(x),dy,y  (dy.*($g))
-    addtest1(f,(-Inf,Inf))
+    addtest1(f,r)
 end
 
 @primitive besselj(nu,x)  error("No gradient for besselj")  error("No gradient for besselj")
@@ -24,14 +24,14 @@ end
 # positive real
 
 # bessel2arg = [
-# (besseli, :((besseli(nu-1,x)+besseli(nu+1,x))/2)),
-# (besselix, :((besselix(nu-1,x)+besselix(nu+1,x))/2)),
-# (besselj, :((besselj(nu-1,x)-besselj(nu+1,x))/2)),
-# (besseljx, :((besseljx(nu-1,x)-besseljx(nu+1,x))/2)),
-# #besselk,
-# #besselkx,
-# #bessely,
-# #besselyx,
+# (besseli, :((besseli(nu-1,x)+besseli(nu+1,x))/2)), # x>=0 || isinteger(nu)
+# (besselix, :((besselix(nu-1,x)+besselix(nu+1,x))/2)), # x>=0 || isinteger(nu)
+# (besselj, :((besselj(nu-1,x)-besselj(nu+1,x))/2)), # x>=0 || isinteger(nu)
+# (besseljx, :((besseljx(nu-1,x)-besseljx(nu+1,x))/2)), # x>=0 || isinteger(nu)
+# #besselk, # x>=0
+# #besselkx, # x>=0
+# #bessely, # x>=0
+# #besselyx, # x>=0
 # ]
 
 # for (f,g) in bessel2arg
