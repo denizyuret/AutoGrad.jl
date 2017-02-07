0f6a337c8944ba3e1d99e1d655cf5baa4c62adfe
Deniz Yuret <denizyuret@gmail.com>
2017-02-07 18:43:44 +0300

    Implemented gradient for vecnorm.

--- src/linalg/generic.jl
+++ src/linalg/generic.jl
@@ -3,6 +3,30 @@ generic2arg = Dict{Symbol,Any}(
 # :\ => (:(-x2./abs2(x1)),:(1./x1)), # (N,*) (V,V) (M,M) (M,V) (V,M)
 )
 
+function vecnormback(x,p,dy,y)
+    if length(x) == 0
+        similar(x)
+    elseif p == 2
+        (dy/y)*x
+    elseif p == 1
+        dy * sign(x)
+    elseif p == Inf
+        dy * sign(x) .* (abs(x) .== y)
+    elseif p == 0
+        zeros(x)
+    elseif p == -Inf
+        dy * sign(x) .* (abs(x) .== y)
+    else
+        (dy*y^(1-p)*(abs(x).^(p-1)).*sign(x))
+    end
+end
+
+@primitive vecnorm(x),dy,y  vecnormback(x,2,dy,y)
+addtest(:vecnorm, rand(2,2)-0.5)
+@primitive vecnorm(x,p::Real),dy,y vecnormback(x,p,dy,y)
+for p in (0,1,2,Inf,-Inf,rand(),-rand(),1+rand(),-1-rand())
+    addtest(:vecnorm, rand(2,2)-0.5, p)
+end
 
 # TODO:
 
