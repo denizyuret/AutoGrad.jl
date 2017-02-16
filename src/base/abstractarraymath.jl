c3b84214c9740ea51018e0057aac6e39dc8f8504
Deniz Yuret <denizyuret@gmail.com>
2017-02-16 23:06:19 +0300

    fixed julia4 ambiguity warnings

--- src/base/abstractarraymath.jl
+++ src/base/abstractarraymath.jl
@@ -20,8 +20,13 @@ addtest(vec,rand(2,2))
 # circshift
 # cumsum_kbn
 # ipermutedims
-@primitive permutedims(x,dims),dy ipermutedims(dy,dims)
-@primitive ipermutedims(x,dims),dy permutedims(dy,dims)
+let perm_r=recorder(permutedims), iperm_r=recorder(ipermutedims)
+    global permutedims, ipermutedims
+    permutedims(x::Rec,dims) = perm_r(x,dims)
+    ipermutedims(x::Rec,dims) = iperm_r(x,dims)
+    permutedims(::Type{Grad{1}},dy,y,x::Rec,dims)=ipermutedims(dy,dims)
+    ipermutedims(::Type{Grad{1}},dy,y,x::Rec,dims)=permutedims(dy,dims)
+end
 addtest(permutedims,rand(2,3,4),(3,1,2))
 addtest(ipermutedims,rand(2,3,4),(3,1,2))
 # repmat
