252f319bb7029f8971f07ac7db5abdd60652e3c2
[A[A[A[B[B[Bwilltebbutt <wt0881@my.bristol.ac.uk>
2017-03-22 16:03:52 +0000

    Minor tweaks to comments and layout of src/linalg/matdiv.jl

--- src/linalg/matdiv.jl
+++ src/linalg/matdiv.jl
@@ -1,3 +1,4 @@
+# All of the reverse-mode sensitivities for operations of the form x1 \ x2.
 ldiv2arg = [
     (:\,          :(-A_mul_Bt(At_ldiv_B(x1, dy), y)), :(At_ldiv_B(x1, dy))),
     (:At_ldiv_B,  :(-A_mul_Bt(y, x1 \ dy)),           :(x1 \ dy)),
@@ -8,6 +9,7 @@ ldiv2arg = [
     (:Ac_ldiv_Bc, :(-A_mul_Bc(y, x1 \ dy)),           :(Ac_rdiv_Bc(dy, x1))),
 ]
 
+# All of the reverse-mode sensitivities for operations of the form x1 / x2.
 rdiv2arg = [
     (:/,          :(A_rdiv_Bt(dy, x2)),  :(-At_mul_B(y, A_rdiv_Bt(dy, x2)))),
     (:At_rdiv_B,  :(A_ldiv_Bt(x2, dy)),  :(-At_mul_B(y, A_rdiv_Bt(dy, x2)))),
@@ -18,15 +20,19 @@ rdiv2arg = [
     (:Ac_rdiv_Bc, :(Ac_ldiv_Bc(x2, dy)), :(-Ac_ldiv_Bc(x2, dy) * y)),
 ]
 
-# We are completely agnostic to the type of the argument.
+# Some matrices for unit testing.
 A = randn(2, 2)
 Σ = A'A + UniformScaling(1e-2)
 X = randn(2, 2)
+
+# Primitive definition and unit testing for ldiv operations.
 for (f, g1, g2) in ldiv2arg
     @eval @primitive $f(x1, x2), dy, y $g1 $g2
     addtest(f, Σ, X)
 end
+
+# Primitive definition and unit testing for rdiv operations.
 for (f, g1, g2) in rdiv2arg
     @eval @primitive $f(x1, x2), dy, y $g1 $g2
-    addtest(f, X, A'A + UniformScaling(1e-2))
+    addtest(f, X, Σ)
 end
