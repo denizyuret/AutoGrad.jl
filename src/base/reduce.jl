d44892791cc50016871c543978589a3f559ee3a4
Deniz Yuret <denizyuret@gmail.com>
2017-09-02 10:14:39 +0000

    sumabs fix wip

--- src/base/reduce.jl
+++ src/base/reduce.jl
@@ -1,24 +1,24 @@
 reduce1arg = [
 (:sum,     :(ones(x))),
-(:sumabs_compat,  :(sign_dot(x))),
-(:sumabs2_compat, :(2x)),
+(:sumabs_,  :(sign_dot(x))),
+(:sumabs2_, :(2x)),
 (:prod,    :(y./x)),
 (:maximum, :(y.==x)),
 (:minimum, :(y.==x)),
-(:maxabs_compat,  :(y.==abs_dot(x))),
-(:minabs_compat,  :(y.==abs_dot(x))),
+(:maxabs_,  :(y.==abs_dot(x))),
+(:minabs_,  :(y.==abs_dot(x))),
 ]
 
 if VERSION >= v"0.6-"; @eval begin
-    sumabs_compat(x...)=sum(abs,x...)
-    sumabs2_compat(x...)=sum(abs2,x...)
-    minabs_compat(x...)=minimum(abs,x...)
-    maxabs_compat(x...)=maximum(abs,x...)
+    sumabs_(x...)=sum(abs,x...)
+    sumabs2_(x...)=sum(abs2,x...)
+    minabs_(x...)=minimum(abs,x...)
+    maxabs_(x...)=maximum(abs,x...)
 end; else; @eval begin
-    sumabs_compat(x...) = sumabs(x...)
-    sumabs2_compat(x...) = sumabs2(x...)
-    minabs_compat(x...) = minabs(x...)
-    maxabs_compat(x...) = maxabs(x...)
+    sumabs_(x...) = sumabs(x...)
+    sumabs2_(x...) = sumabs2(x...)
+    minabs_(x...) = minabs(x...)
+    maxabs_(x...) = maxabs(x...)
 end; end
 
 for (f,g) in reduce1arg
@@ -37,6 +37,21 @@ end
 # addtest(:sum, (rand(2)...))
 
 # TODO: implement more general sum ops
+
+if VERSION >= v"0.6-"
+    let sum_r = recorder(sum), max_r = recorder(maximum), min_r = recorder(minimum)
+        global sum, maximum, minimum
+        sum(f::typeof(abs), x::Rec, r...) = sum_r(f, x, r...)
+        sum(f::typeof(abs2), x::Rec, r...) = sum_r(f, x, r...)
+        maximum(f::typeof(abs), x::Rec, r...) = max_r(f, x, r...)
+        minimum(f::typeof(abs), x::Rec, r...) = min_r(f, x, r...)
+        sum(Grad{2},dy,y,f::typeof(abs),x,r...) = sumabs(Grad{1},dy,y,x,r...)
+        sum(Grad{2},dy,y,f::typeof(abs2),x,r...) = sumabs2(Grad{1},dy,y,x,r...)
+        maximum(Grad{2},dy,y,f::typeof(abs),x,r...) = maxabs(Grad{1},dy,y,x,r...)
+        minimum(Grad{2},dy,y,f::typeof(abs),x,r...) = minabs(Grad{1},dy,y,x,r...)
+    end
+end
+
 # TODO: other functions in reduce.jl:
 # r_promote: Not exported
 # mapfoldl_impl: Not exported
