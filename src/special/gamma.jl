587ce80ae31c89c96b747b51860763f4cb492d08
Deniz Yuret <denizyuret@gmail.com>
2017-09-28 19:59:38 +0300

    SpecialFunctions pass julia6 tests

--- src/special/gamma.jl
+++ src/special/gamma.jl
@@ -1,13 +1,21 @@
 gamma1arg = [
 (:gamma, :(y.*digamma_dot(x)), (-Inf,Inf)),
-(:lfact, :(sign_dot(y).*digamma_dot(x+1)), (-Inf,Inf)),
+# (:lfact, :(sign_dot(y).*digamma_dot(x+1)), (-Inf,Inf)), # lfact only defined for integers
 (:lgamma, :(digamma_dot(x)), (-Inf,Inf)),
 (:digamma, :(trigamma_dot(x)), (-Inf,Inf)), # polygamma(0,x)
-(:trigamma, :(polygamma(2,x)), (-Inf,Inf)), # polygamma(1,x)
+(:trigamma, :(polygamma2_dot(x)), (-Inf,Inf)), # polygamma(1,x)
 (:invdigamma, :(1./trigamma_dot(y)), (-Inf,Inf)),
+(:polygamma2, :(error()), (-Inf,Inf)),
 # zeta: TODO. Riemann 1-arg zeta
 # eta # TODO. related to zeta
 ]
+
+polygamma2(x)=polygamma(2,x)
+if VERSION >= v"0.6.0"
+    polygamma2_dot(x)=polygamma2.(x)
+else
+    polygamma2_dot(x)=polygamma2(x)
+end
 
 for (f,g,r) in gamma1arg
     bf = broadcast_func(f)
@@ -15,7 +23,9 @@ for (f,g,r) in gamma1arg
     if bf != f
         @eval @primitive $bf(x),dy,y  (dy.*($g))
     end
-    addtest1(f,r)
+    if f != :polygamma2
+        addtest1(f,r)
+    end
 end
 
 gamma2arg = [
@@ -26,6 +36,7 @@ gamma2arg = [
 
 # polygamma wants x1 to be a non-negative integer, x2 unrestricted
 @primitive polygamma(x1,x2),dy,y  nothing  unbroadcast(x2,dy.*polygamma(x1+1,x2))
-polygamma2(x,i)=polygamma(i,x)
-addtest(:polygamma2, randn(), rand(0:5))
+polygamma_(x,i)=polygamma(i,x)
+addtest(:polygamma_, randn(), rand(0:5))
 # TODO: add broadcasting version
+
--- test/primitives.jl
+++ test/primitives.jl
@@ -2,7 +2,7 @@ include("header.jl")
 
 @testset "primitives" begin
     for t in AutoGrad.alltests()
-        @show t
+        #@show t
         @test gradcheck(eval(AutoGrad,t[1]), t[2:end]...)
     end
 end
