ce86cc5d5ca85ca76f3cee11e4e98e945e0cf22b
CarloLucibello <carlo.lucibello@gmail.com>
2018-02-20 10:51:30 -0500

    cholesky

--- src/linalg/dense.jl
+++ src/linalg/dense.jl
@@ -37,3 +37,21 @@ dense2arg = Dict{Symbol,Any}(
 # cond
 # sylvester
 # lyap
+
+@primitive chol(x),dy,y   chol_back(y, dy)
+
+chol_ϕ(A) = tril(A) - 0.5diagm(diag(A))
+
+# ref: Iain Murray's https://arxiv.org/pdf/1602.07527.pdf
+# difference with the paper with respect to the paper:
+# julia L is upper triangular and we do not need the
+# final simmetrization of S
+function chol_back(L, dL)
+    dL = triu(dL)
+    iL = inv(L)
+    S = iL * chol_ϕ(L*dL') * iL'
+    #  S + S' - diagm(diag(S))
+    S
+end
+
+
--- test/primitives.jl
+++ test/primitives.jl
@@ -1,6 +1,8 @@
 include("header.jl")
 
 @testset "primitives" begin
+    @test gradcheck(x->chol(x'x), rand(3,3))
+    
     for t in AutoGrad.alltests()
         #@show t
         @test gradcheck(eval(AutoGrad,t[1]), t[2:end]...)
