959e191e67319a360a288d743f1863687e0673f2
Rene Donner <code@donner.at>
2018-04-16 10:33:17 +0200

    typo fix

--- test/primitives.jl
+++ test/primitives.jl
@@ -1,7 +1,7 @@
 include("header.jl")
 
 @testset "primitives" begin
-for t in AutoGrad.alltests()
+    for t in AutoGrad.alltests()
         @test gradcheck(eval(AutoGrad,t.f), t.args...; kwargs = t.kargs)
     end
 end
