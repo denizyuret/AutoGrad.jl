19a10044023229aaf4eabfd485144d15483fad9b
ekinaykurek <akyurekekin@gmail.com>
2018-08-17 17:12:17 +0300

    variables and functions are inside of @testset

--- test/linearalgebra.jl
+++ test/linearalgebra.jl
@@ -1,35 +1,36 @@
 include("header.jl")
 using LinearAlgebra
 
-w = randn(3,2)
-wt = randn(2,3)
-wsquare = randn(3,3)
-udg = [1.,2.,3.]
-dg = [4.,5.,6.]
-ldg  = [7.,8.,9.]
-
-diagm1(x,y,z) = diagm(1=>x,0=>y,-1=>z)
-
-function krontest(w1,w2)
-    sum(kron(w1,w2))
-end
-
-function qrtest(w) 
-    Q,R = qr(w)
-    sum(Q) + 2sum(R)
-end
-
-function lqtest(w) 
-    Q,L = lq(w)
-    sum(Q) + 2sum(L)
-end
-
-function svdtest(w) 
-    U,S,V = svd(w)
-    sum(U) + 2sum(S) + 3sum(V)
-end
-
 @testset "LinearAlgebra" begin
+
+    w = randn(3,2)
+    wt = randn(2,3)
+    wsquare = randn(3,3)
+    udg = [1.,2.,3.]
+    dg = [4.,5.,6.]
+    ldg  = [7.,8.,9.]
+
+    diagm1(x,y,z) = diagm(1=>x,0=>y,-1=>z)
+
+    function krontest(w1,w2)
+        sum(kron(w1,w2))
+    end
+
+    function qrtest(w) 
+        Q,R = qr(w)
+        sum(Q) + 2sum(R)
+    end
+
+    function lqtest(w) 
+        Q,L = lq(w)
+        sum(Q) + 2sum(L)
+    end
+
+    function svdtest(w) 
+        U,S,V = svd(w)
+        sum(U) + 2sum(S) + 3sum(V)
+    end
+
     @test gradcheck(adjoint,w)
     @test gradcheck(det,wsquare)
     #@test gradcheck(diag,w)
