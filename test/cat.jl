c5b3e37edef53b15f47ad7bd4ad3000dc1e5aefa
ekinaykurek <akyurekekin@gmail.com>
2018-08-17 17:25:44 +0300

    variables and functions are inside of @testset

--- test/cat.jl
+++ test/cat.jl
@@ -1,15 +1,16 @@
 include("header.jl")
-cat1(x...)=cat(x...; dims=Val(1))
-cat2(x...)=cat(x...; dims=Val(2))
-cat31(x...)=cat(x...; dims=Val(1))
-cat32(x...)=cat(x...; dims=Val(2))
-cat33(x...)=cat(x...; dims=Val(3))
-
-x1d = (1.,2.)
-x2d = ([1. 2.],[3. 4.]) 
-x3d = (randn(2,3,5),randn(1,3,5),randn(2,2,5),randn(2,3,2)) 
 
 @testset "cat" begin
+    cat1(x...)=cat(x...; dims=Val(1))
+    cat2(x...)=cat(x...; dims=Val(2))
+    cat31(x...)=cat(x...; dims=Val(1))
+    cat32(x...)=cat(x...; dims=Val(2))
+    cat33(x...)=cat(x...; dims=Val(3))
+
+    x1d = (1.,2.)
+    x2d = ([1. 2.],[3. 4.]) 
+    x3d = (randn(2,3,5),randn(1,3,5),randn(2,2,5),randn(2,3,2)) 
+
     @test gradcheckN(cat1, x1d...)
     #@test gradcheckN(cat1, x1d[1], x2d[1]) #uncat mismatch error
     
