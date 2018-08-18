d604b43a63f9cd782aec2629f3a6e93ca21ca7c7
ekinaykurek <akyurekekin@gmail.com>
2018-08-18 19:46:23 +0300

    more iterate tests

--- test/iterate.jl
+++ test/iterate.jl
@@ -1,8 +1,9 @@
 include("header.jl")
 
 @testset "iterate" begin
-    w = [randn(2,3,5),randn(1,3,5),randn(2,2,5),randn(2,3,2)]
-    wdict = Dict(:w1=>randn(2,3),:w2=>randn(3,2))
+    warray  = [randn(2,3,5),randn(1,3,5)]
+    wtuple  = (randn(2,3,5),randn(1,3,5))
+    wdict   = Dict(:w1=>randn(2,3,5),:w2=>randn(1,3,5))
 
     function itr1(w)
         total = 0.0
@@ -21,12 +22,17 @@ include("header.jl")
         for (k,wi) in w; total+=sum(wi); end
         return total
     end
+
+    function itr4(w)
+        w1,w2 = w
+        return sum(w1)+sum(w2)
+    end
     
-
-    @test gradcheck(itr1,w)
-    @test gradcheck(itr2,w)
-    @test gradcheck(itr1,w[1])
-    @test gradcheck(itr2,w[1])
+    @test gradcheck(itr1,warray)
+    @test gradcheck(itr2,warray)
+    @test gradcheck(itr1,warray[1])
+    @test gradcheck(itr2,warray[1])
     @test gradcheck(itr3,wdict) #fails
-
+    @test gradcheck(itr4,warray) 
+    @test gradcheck(itr4,wtuple)
 end
