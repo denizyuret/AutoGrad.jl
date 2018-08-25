e6882d7ac9897b829b6c68fbe821edf2888f0bd4
Deniz Yuret <denizyuret@gmail.com>
2018-08-25 07:32:09 +0300

    dictionary based tape

--- src/core.jl
+++ src/core.jl
@@ -2,11 +2,11 @@ export Param, differentiate, df, gradient
 
 abstract type Rec{T} end
 
-struct Param{T} <: Rec{T}
+mutable struct Param{T} <: Rec{T}
     value::T
 end
 
-struct Result{T} <: Rec{T}
+mutable struct Result{T} <: Rec{T}
     value::T
     func::Function
     args::Tuple
@@ -18,46 +18,63 @@ mutable struct Node
     outgrad
     rec::Rec
     parents::Vector{Node}
-    Node(r::Result) = new(nothing, r, Array{Node}(undef, length(r.args)))
-    Node(r::Param) = new(nothing, r, Array{Node}[])
+    prev::Node
+    Node(og,rc,pa,pr)=new(og,rc,pa,pr)
+    Node()=new()
 end
 
-const Tape = Vector{Node}
+# Tape: rec0=>node0, rec1=>node1, ..., recN=>nodeN
+# where node[n].prev = node[n-1]
+# Special node0 marks both ends of the tape: 
+# node1.prev = node0
+# node0.prev = nodeN
+# Special rec0 acts as a key to node0.
+# mutable struct is a lot faster as key for an IdDict!
+const Tape = IdDict{Rec,Node}
+const EOT = Param([])
+newtape() = (n=Node(); n.prev=n; Tape(EOT => n))
+# Tape is iterated in reverse, last in first out
+# This also makes first(tape) the final result node
+Base.iterate(t::Tape,s=(t[EOT],t[EOT])) = 
+    ((p,n) = s; p = p.prev; p === n ? nothing : (p, (p, n)))
+# This hack is to make grad faster:
+last(t::Tape)=t[EOT].parents[1]
+
+gradient(t,x)=nothing
+gradient(t::Tape,x::Rec)=(n=get(t,x,nothing); n===nothing ? n : n.outgrad)
 
 getval(x)=x
 getval(x::Rec)=x.value
-getval(x::Tape)=x[end].rec.value
+getval(x::Tape)=first(x).rec.value
 
-global _tapes = []
+_tapes = Tape[]
 
 function differentiate(f, x...; o...)
     global _tapes
-    tape = Tape()
+    tape = newtape()
     push!(_tapes, tape)
     result = nothing
     try
         result = f(x...; o...)
     catch e
-        pop!(_tapes)
-        throw(e)
+        pop!(_tapes); throw(e)
     end
     if pop!(_tapes) !== tape; error("Tape stack error"); end
     if !isa(result,Result); return result; end
-    if !isa(result.value, Number); error("diff can only handle scalar valued functions"); end
-    if result !== tape[end].rec; error("Result not on tape"); end
-
-    tape[end].outgrad = one(result.value)
-    for j = length(tape):-1:1
-        n = tape[j]
+    if !isa(result.value, Number); error("AutoGrad can only handle scalar valued functions"); end
+    n = first(tape)
+    if result !== n.rec; error("Result not on tape"); end
+    n.outgrad = one(result.value)
+    for n in tape
         if n.outgrad == nothing; continue; end
         r = n.rec
-        for i in 1:length(n.parents)
+        @inbounds for i in 1:length(n.parents)
             if !isassigned(n.parents, i); continue; end
             p = n.parents[i]
             g = back(r.func, Val(i), n.outgrad, r, r.args...; r.kwargs...)
             p.outgrad = sum_outgrads(p.outgrad, g)
         end
-        #if !isa(r,Param); tape[j] = nothing; end
+        #if !isa(r,Param); n.outgrad = nothing; end
     end
     return tape
 end
@@ -65,60 +82,47 @@ end
 const df = differentiate
 
 function forw(f, args...; kwargs...)
-    global _tapes
     argvals = getval.(args)
     result = f(argvals...; kwargs...)
     if isempty(_tapes); return result; end
     result = Result(result, f, args...; kwargs...)
     for tape in _tapes
-        rnode = Node(result)
-        for argnum = 1:length(args)
-            arg = args[argnum]
-            if !isa(arg,Rec); continue; end
-            parent = findnode!(tape,arg)
-            rnode.parents[argnum] = parent
-        end
-        push!(tape, rnode)
+        push!(tape, result)
     end
     return result
 end
 
-function findnode(t::Tape,x::Rec)
-    for n in t
-        if n.rec === x
-            return n
+function Base.push!(t::Tape, r::Rec)
+    n = get(t,r,nothing)
+    if n !== nothing; return n; end
+    if isa(r,Result)
+        nargs = length(r.args)
+        parents = Array{Node}(undef, nargs)
+        @inbounds for argnum = 1:nargs
+            arg = r.args[argnum]
+            if !isa(arg,Rec); continue; end
+            parent = push!(t,arg)
+            parents[argnum] = parent
         end
+    else
+        parents = Node[]
     end
-    return nothing
+    m = t[EOT]
+    prev = m.prev
+    node = Node(nothing, r, parents, prev)
+    if !isdefined(m,:parents); m.parents = [node]; end
+    t[r] = m.prev = node
 end
 
-function findnode!(t::Tape,x::Rec)
-    for n in t
-        if n.rec === x
-            return n
-        end
-    end
-    #TODO: do we always push all nodes?
-    node = Node(x)
-    push!(t, node)
-    return node
-end
-
-function gradient(t::Tape,x::Rec)
-    n = findnode(t,x)
-    n == nothing ? n : n.outgrad
-end
-
-gradient(t,x)=nothing
-
+# old interface
 function grad(fun::Function, argnum::Int=1)
     function gradfun(args...; kwargs...)
         arg_wrt = args[argnum]
-        arg_wrt = isa(arg_wrt,Rec) ? identity(arg_wrt) : Param(arg_wrt) # identity(arg_wrt) from PR#75
+        arg_wrt = !isa(arg_wrt,Rec) ? Param(arg_wrt) : identity(arg_wrt) # from PR#75
         args = Any[args...]
         args[argnum] = arg_wrt
         result = differentiate(fun, args...; kwargs...)
-        isa(result, Tape) ? result[1].outgrad : nothing
+        isa(result, Tape) ? last(result).outgrad : nothing
     end
     return gradfun
 end
