b096803243e816710d48d9edcb8b423f3a6bb9e8
Deniz Yuret <denizyuret@gmail.com>
2018-08-27 10:57:52 -0400

    export abbrevs by default

--- src/AutoGrad.jl
+++ src/AutoGrad.jl
@@ -1,7 +1,7 @@
 module AutoGrad
 
 using LinearAlgebra, Statistics, SpecialFunctions
-export Param, differentiate, gradient, value, grad, gradloss, getval
+export Param, differentiate, gradient, value, grad, gradloss, getval, gr, df, pa
 export @primitive, @zerograd, @primitive1, @zerograd1
 
 include("core.jl")
@@ -20,20 +20,15 @@ include("show.jl")
 include("../test/gradcheck.jl")
 #include("../util/debug.jl")
 
-"Use `using AutoGrad.Abbrev` to get the shortcuts gr, df, pa."
-module Abbrev
-using ..AutoGrad
-const gr = gradient
-const df = differentiate
-const pa = Param
-export gr,df,pa
-end
-
 "Use `AutoGrad.dir(path...)` to construct paths relative to AutoGrad root."
 dir(path...) = joinpath(dirname(dirname(@__FILE__)),path...)
 
 # Deprecations:
 @deprecate Rec Value
 @deprecate getval value
+
+const gr = gradient
+const df = differentiate
+const pa = Param
 
 end # module
