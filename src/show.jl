3530e17012087599ae34ffe2b5728acfdfabea26
Deniz Yuret <denizyuret@gmail.com>
2018-09-17 00:07:17 -0400

    show improvements

--- src/show.jl
+++ src/show.jl
@@ -8,10 +8,16 @@
 
 import Base: show
 
+# Summary string
+valstr(x)=_valstr(value(x))
+_valstr(::Nothing)="nothing"
+_valstr(x)=(hasmethod(size,(typeof(x),)) && !isempty(size(x)) ? "$(typeof(x))($(join(size(x),',')))" : string(x))
+_valstr(t::Tuple)="($(join(_valstr.(t),',')))"
+
 # One line show used for show, print, string etc.
-show(io::IO, x::Param)  = print(IOContext(io,:compact=>true), "P(", value(x), ")")
-show(io::IO, x::Result) = print(IOContext(io,:compact=>true), "R(", value(x), ")")
-show(io::IO, x::Tape)   = print(IOContext(io,:compact=>true), "T(", value(x), ")")
+show(io::IO, x::Param)  = print(IOContext(io,:compact=>true), "P(", valstr(x), ")")
+show(io::IO, x::Result) = print(IOContext(io,:compact=>true), "R(", valstr(x), ")")
+show(io::IO, x::Tape)   = print(IOContext(io,:compact=>true), "T(", valstr(x), ")")
 
 # Multi line show used for display:
 show(io::IO, ::MIME"text/plain", x::Tape) = show(io, x)
@@ -26,29 +32,29 @@ summary(io::IO, x::Value{A}) where {A<:AbstractArray} = print(io, Base.dims2stri
 summary(io::IO, p::ArrayValue) = summary(io, p.p)
 
 function show(io::IO, n::Node)
-    og(n::Node)=(n.outgrad === nothing ? 0 : n.outgrad)
+    og(n::Node)=(n.outgrad === nothing ? 0 : valstr(n.outgrad))
     io = IOContext(io,:compact=>true)
     if isa(n.Value, Param)
         print(io, "N(", og(n), ", ", n.Value, ")")
     else
         r = n.Value
-        print(io, "N(", og(n), ", ", r, " = ", r.func, (r.args..., r.kwargs...))
+        print(io, "N(", og(n), ", ", r, " = ", r.func, "(", join(valstr.(r.args),", "), 
+              isempty(r.kwargs) ? "" : "; "*join(["$(x[1])=$(valstr(x[2]))" for x in r.kwargs], ", "), "))")
     end
 end
 
 function show(io::IO, ::MIME"text/plain", ts::Vector{Tape}) # to dump _tapes
     if isempty(ts); show(io, ts); end
-    og(t::Tape,r::Value)=(n=get(t,r,nothing); n===nothing ? '-' : n.outgrad===nothing ? '0' : n.outgrad)
+    og(t::Tape,r::Value)=(n=get(t,r,nothing); n===nothing ? '-' : n.outgrad===nothing ? '0' : valstr(n.outgrad))
     io = IOContext(io,:compact=>true)
-    for n in reverse(collect(ts[1]))
+    for (i,n) in enumerate(reverse(collect(ts[1])))
         r = n.Value
-        for t in ts
-            print(io, og(t,r), "\t")
-        end
         if isa(r,Result)
-            println(io, r, " = ", r.func, (r.args..., r.kwargs...))
+            print(io, "$i. ", valstr(r), " = ", r.func, "(", join(valstr.(r.args),", "), 
+                  isempty(r.kwargs) ? "" : "; "*join(["$(x[1])=$(valstr(x[2]))" for x in r.kwargs], ", "), "))")
         else
-            println(io, r)
+            print(io, "$i. ", r)
         end
+        println(io, " ∇=", join([og(t,r) for t in ts], '|'))
     end
 end
