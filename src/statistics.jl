a0fe12d7cd85d1a2566298fc8f5a8a6eef7dfe72
Deniz Yuret <denizyuret@gmail.com>
2019-09-16 09:16:09 +0300

    Added support for varm and stdm.

--- src/statistics.jl
+++ src/statistics.jl
@@ -1,6 +1,6 @@
-# Completed: 6/13
+# Completed: 8/13
 
-import Statistics: mean, var, std
+import Statistics: mean, var, std, varm, stdm
 
 # TODO:
 # cor
@@ -13,18 +13,23 @@ import Statistics: mean, var, std
 # quantile
 # quantile! overwrites.
 # std done.
-# stdm
+# stdm done.
 # var done.
-# varm
+# varm done.
 
 @primitive mean(x;d...),dy  (dy.*one.(x).*length(dy)./length(x))
 @primitive mean(f::typeof(abs),x;d...),dy   nothing  (dy.*sign.(x).*length(dy)./length(x))
 @primitive mean(f::typeof(abs2),x;d...),dy  nothing  (dy.*(2x).*length(dy)./length(x))
 
-function var(x::Value; dims=:, mean=mean(x, dims=dims), corrected=true)
-    s = sum(abs2, x .- mean, dims=dims)
-    a = length(x) ÷ length(s) 
-    corrected ? s ./ (a-1) : s ./ a  
+function varm(x::Value, m; corrected::Bool=true, dims=:)
+    s = sum(abs2, x .- m; dims=dims)
+    r = length(x) ÷ length(s) - Int(corrected)
+    s ./ r
+end
+
+function var(x::Value; corrected::Bool=true, mean=nothing, dims=:)
+    varm(x, something(mean, Statistics.mean(x,dims=dims)); corrected=corrected, dims=dims)
 end
 
 std(x::Value, args...; kws...) = sqrt.(var(x, args...; kws...))
+stdm(x::Value, args...; kws...) = sqrt.(varm(x, args...; kws...))
