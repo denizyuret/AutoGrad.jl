c1597882180b1b02c55c01b96fe5b02ffb7f44fc
Deniz Yuret <denizyuret@gmail.com>
2019-10-13 09:25:50 +0300

    tests for #114

--- test/sparse.jl
+++ test/sparse.jl
@@ -1,7 +1,8 @@
 include("header.jl")
 
 @testset "sparse" begin
-    # Issue 114
+
+    # Issue #114 (a): plus for Sparse.
     using AutoGrad: Sparse, full, addto!
     a = Sparse(zeros(3,4), [ [1.,1.], [1.,1.], 1., 1. ], [ ([1,2],), (3:4,), (2,2), (1,) ])
     b = a + a
@@ -13,4 +14,12 @@ include("header.jl")
     b = a + a
     a .+= a
     @test full(a) == full(b)
+
+    # Issue #114 (b): lmul! ambiguous for Sparse, breaks gclip.
+    using LinearAlgebra
+    foo(w) = (s = 0.0; for i=1:length(w); s+=w[i]; end; s)
+    w = Param(randn(2,2))
+    J = @diff foo(w)
+    @test lmul!(0.5, grad(J,w)) isa Sparse
+    
 end
